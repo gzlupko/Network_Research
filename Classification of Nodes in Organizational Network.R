924fe1818dab2566e85c0f1cf45f9586f85d142d
gzlupko <69553827+gzlupko@users.noreply.github.com>
2020-08-17 17:52:06 -0400

    EDA plots

--- Classification of Nodes in Organizational Network.R
+++ Classification of Nodes in Organizational Network.R
@@ -11,6 +11,7 @@ library(igraphdata)
 library(igraph)
 library(tidygraph) 
 library(ggraph) 
+library(gridExtra) 
 
 
 # import data set 
@@ -95,7 +96,7 @@ nodes_with_levels %>%
 
 # further clean up. Break into 4 layers - 
 # employee, associate, director, executive
-# Put lawyer in executive 
+# Put lawyer in executive and arbitrarily assign NA and Trader to 'associate'
 
 View(nodes_with_levels) 
 nodes_with_levels$JobLevel[61] <- "Executive"
@@ -112,6 +113,16 @@ nodes_with_levels <- nodes_with_levels %>%
   
 
 table(nodes_with_levels$JobLevel) 
+
+
+# visualize new job levels to be used for analyses 
+
+nodes_with_levels %>%
+  group_by(JobLevel) %>%
+  count(JobLevel) %>%
+  ggplot(aes(JobLevel, n, fill = JobLevel)) + geom_bar(stat = "identity") +
+  ylab("") + theme(legend.position = "none") 
+  
 
 # start by breaking into  4 job levels
 # arbitrarily assigning Trader and NA to level 2 just above employee
@@ -130,20 +141,52 @@ head(nodes_filtered)
 
 # EDA of network structure characteristics by job level 
 
+View(nodes_filtered)
+table(nodes_filtered$JobLevel)
+
+level_summary <- nodes_filtered %>%
+  group_by(JobLevel) %>%
+  summarize(avg_in_degree = mean(in_degree), 
+            avg_out_degree = mean(out_degree), 
+            avg_strength = mean(strength), 
+            avg_between = mean(between)) 
+
+head(level_summary)
+avg_in_degree <- ggplot(data = level_summary, 
+                        aes(x = JobLevel, y = avg_in_degree, fill = JobLevel)) + 
+  geom_bar(stat = "identity") + ylab("") + theme(legend.position = "none") + 
+  ggtitle("Avg. In-Degree") 
+
+avg_out_degree <- ggplot(data = level_summary, 
+                        aes(x = JobLevel, y = avg_out_degree, fill = JobLevel)) + 
+  geom_bar(stat = "identity") + ylab("") + theme(legend.position = "none") + 
+  ggtitle("Avg. Out-Degree")
+
+avg_strength <- ggplot(data = level_summary, 
+                        aes(x = JobLevel, y = avg_strength, fill = JobLevel)) + 
+  geom_bar(stat = "identity") + ylab("") + theme(legend.position = "none") + 
+  ggtitle("Avg. Strength")
+
+avg_between <- ggplot(data = level_summary, 
+                        aes(x = JobLevel, y = avg_between, fill = JobLevel)) + 
+  geom_bar(stat = "identity") + ylab("") + theme(legend.position = "none") +
+  ggtitle("Avg. Betweenness") 
 
 
+grid.arrange(avg_in_degree, avg_out_degree, avg_strength, avg_between)
 
 
-
-
-
-
+# check for statistical significance
 
 
 
 
 
 # kNN - can model predict job level my measures of network centrality in organization?
+
+
+
+
 
 
 
